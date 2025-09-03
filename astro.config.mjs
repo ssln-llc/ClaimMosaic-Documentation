3c8b5089073240569d542da516a23a80f9b3b58a
Scott Scowden <scott@graydogsolutions.com>
2025-09-02 22:37:01 -0400

    Index & Favicon updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,19 @@ export default defineConfig({
   integrations: [
       starlight({
           title: 'ClaimMosaic Documentation',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/ssln-llc/ClaimMosaic-Documentation' }],
+          head: [
+            { tag: 'link', attrs: { rel: 'icon', href: '/favicon.ico' } },
+            { tag: 'link', attrs: { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' } },
+            { tag: 'link', attrs: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' } },
+            { tag: 'link', attrs: { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' } },
+            { tag: 'link', attrs: { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' } },
+            { tag: 'link', attrs: { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' } },
+            { tag: 'link', attrs: { rel: 'manifest', href: '/site.webmanifest' } },
+            { tag: 'meta', attrs: { name: 'msapplication-TileColor', content: '#da532c' } },
+            { tag: 'meta', attrs: { name: 'msapplication-config', content: '/browserconfig.xml' } },
+            { tag: 'meta', attrs: { name: 'theme-color', content: '#ffffff' } },
+          ],
           sidebar: [
               {
                   label: 'Getting Started',
