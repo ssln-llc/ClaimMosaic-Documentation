26a51d76ae4c205e149d694327403eee4c7239ec
Scott Scowden <scott@graydogsolutions.com>
2025-09-02 22:15:48 -0400

    Update homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -58,5 +60,7 @@ export default defineConfig({
               }
           ],
       }),
-	],
+    ],
+
+  adapter: cloudflare(),
 });
